a5709e33797049872fa70ca624fe03979ed2a27d
ThongBn <thongbn@outlook.com>
2022-11-01 21:33:46 +0700

    update jenkins file

--- backend/config/middlewares.js
+++ backend/config/middlewares.js
@@ -1,12 +1,43 @@
-module.exports = [
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::logger',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
-];
+module.exports = ({env}) => {
+
+  const origin = [
+    'http://localhost:1337'
+    , 'http://localhost:8000'
+  ];
+
+  origin.push(...env('CLIENT_DOMAIN', '').split(","));
+
+  return [
+    'strapi::errors',
+    {
+      name: 'strapi::security',
+      config: {
+        contentSecurityPolicy: {
+          useDefaults: true,
+          directives: {
+            'script-src': ["'self'", "'unsafe-inline'", 'cdn.jsdelivr.net', "maps.googleapis.com", "editor.unlayer.com"],
+            'img-src': ["'self'", 'data:', 'blob:', 'cdn.jsdelivr.net', 'strapi.io', "maps.gstatic.com", "maps.googleapis.com", "editor.unlayer.com"],
+            'media-src': ["'self'", 'data:', 'blob:', "maps.gstatic.com", "maps.googleapis.com", "editor.unlayer.com"],
+            'frame-src': ["'self'", "editor.unlayer.com"],
+            upgradeInsecureRequests: null,
+          },
+        },
+      },
+    },
+    'strapi::poweredBy',
+    {
+      name: 'strapi::cors',
+      config: {
+        enabled: true,
+        headers: '*',
+        origin
+      }
+    },
+    'strapi::logger',
+    'strapi::query',
+    'strapi::body',
+    'strapi::session',
+    'strapi::favicon',
+    'strapi::public',
+  ];
+}
