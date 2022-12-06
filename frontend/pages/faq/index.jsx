e2603940da93d7de6e30cbffb800d36f8d6c850c
ThongBn <thongbn@outlook.com>
2022-12-06 11:59:05 +0700

    Faq

--- frontend/pages/faq/index.jsx
+++ frontend/pages/faq/index.jsx
@@ -6,9 +6,8 @@ import {useTranslation} from "next-i18next";
 import DecorComponent from "../../components/DecorComponent";
 import {callGet} from "../../ulti/helper";
 
-const Page = ({models = []}) => {
+const Page = ({models}) => {
     const {t} = useTranslation("common");
-    const [faqs, setFaqs] = useState(models);
 
     return <PageLayout>
         <Container className="gallery-section">
@@ -17,36 +16,16 @@ const Page = ({models = []}) => {
             </div>
             <div className="mt-4">
                 <Accordion defaultActiveKey="0">
-                    <Accordion.Item eventKey="0">
-                        <Accordion.Header><span>Q.</span> Can I cancel my tickets?</Accordion.Header>
-                        <Accordion.Body>
-                            <span>A.</span> Hop-on hop-off tickets can be cancelled up to 24 hours before the date selected at the time
-                            of purchase. If you request a refund less than 24 hours before the date stated on your
-                            voucher, you will not be entitled to a refund.
-                            Combination tickets cannot be cancelled. All sales are final. A combination ticket is a
-                            ticket that includes a hop-on hop-off bus and other product, as well as an entry ticket to a
-                            local attraction or tour.
-                            Attractions can be cancelled depending on the cancellation policy stipulated by each
-                            attraction.We are not responsible if an attraction does not give refunds if a customer is
-                            not able to use the ticket. Please check an attraction’s cancellation policy before making
-                            the request.
-                        </Accordion.Body>
-                    </Accordion.Item>
-                    <Accordion.Item eventKey="1">
-                        <Accordion.Header><span>Q.</span> Can I cancel my tickets?</Accordion.Header>
-                        <Accordion.Body>
-                            <span>A.</span> Hop-on hop-off tickets can be cancelled up to 24 hours before the date selected at the time
-                            of purchase. If you request a refund less than 24 hours before the date stated on your
-                            voucher, you will not be entitled to a refund.
-                            Combination tickets cannot be cancelled. All sales are final. A combination ticket is a
-                            ticket that includes a hop-on hop-off bus and other product, as well as an entry ticket to a
-                            local attraction or tour.
-                            Attractions can be cancelled depending on the cancellation policy stipulated by each
-                            attraction.We are not responsible if an attraction does not give refunds if a customer is
-                            not able to use the ticket. Please check an attraction’s cancellation policy before making
-                            the request.
-                        </Accordion.Body>
-                    </Accordion.Item>
+                    {models.data.map((item, idx) => (
+                        <Accordion.Item key={`faq_${idx}`} eventKey={`faq_${idx}`}>
+                            <Accordion.Header><span>Q.</span> {item.attributes.question}</Accordion.Header>
+                            <Accordion.Body>
+                                <div dangerouslySetInnerHTML={{
+                                    __html: item.attributes.answer
+                                }}/>
+                            </Accordion.Body>
+                        </Accordion.Item>
+                    ))}
                 </Accordion>
             </div>
         </Container>
@@ -55,21 +34,34 @@ const Page = ({models = []}) => {
 };
 
 export const getServerSideProps = async (context) => {
-    const {locale = 'vi'} = context;
+    const {locale = 'vi', query} = context;
 
-    let models = [];
+    let model = {};
     try {
         const res = await callGet("/faqs", {
             sortBy: ['id:desc'],
+            pagination: {
+                page: query.page ? query.page : 1,
+                pageSize: query.pageSize ? query.pageSize : 20
+            }
         });
-        models = res.data;
+        model = res;
     } catch (e) {
         console.error(e);
+        model = {
+            data: [],
+            meta: {
+                pagination: {
+                    page: 1,
+                    pageCount: 0
+                }
+            }
+        }
     }
 
     return {
         props: {
-            models,
+            model,
             ...(await serverSideTranslations(locale, ['common'])),
         },
     }
