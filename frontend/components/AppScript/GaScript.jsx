8c7b6f0805eb34cf27395d8717d1f4c926d722b4
ThongBn <thongbn@outlook.com>
2023-02-11 17:30:57 +0700

    Ga fix

--- frontend/components/AppScript/GaScript.jsx
+++ frontend/components/AppScript/GaScript.jsx
@@ -2,25 +2,29 @@ import React from "react";
 import Script from "next/script";
 
 const GaScript = ({gaTags = []}) => {
-    return <>
-        {gaTags.map((item, idx) => {
-            return <>
-                <Script
-                    src={`https://www.googletagmanager.com/gtag/js?id${item}`}
-                    strategy="afterInteractive"
-                />
-                <Script id="google-analytics" strategy="afterInteractive">
-                    {`
+
+    const GaTag = ({gaId}) => {
+        return <>
+            <Script
+                src={`https://www.googletagmanager.com/gtag/js?id${gaId}`}
+                strategy="afterInteractive"
+            />
+            <Script id="google-analytics" strategy="afterInteractive">
+                {`
                       window.dataLayer = window.dataLayer || [];
                       function gtag(){window.dataLayer.push(arguments);}
                       gtag('js', new Date());
             
-                      gtag('config', "${item}");
+                      gtag('config', "${gaId}");
                     `}
-                </Script>
-            </>
-        })}
-    </>
+            </Script>
+        </>
+    };
+
+    return gaTags.map((item, idx) => {
+        return <GaTag gaId={item} key={`ga_${idx}`}/>
+    })
+
 };
 
 export default GaScript;
